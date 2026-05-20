4376fb54b44d8fc6e6fd7d3d489620bddf54c78a
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-05-20 10:16:06 +0200

    solved exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+	
+	if ((num1 || num2) < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+		return ("ERROR");
+	let minValue = Math.min(num1, num2);
+	let maxValue = Math.max(num1, num2);
+	let result = 0;
+
+	for (let i = minValue; i <= maxValue; i++)	
+		result += i;
+
+	return (result);
 };
 
+
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
